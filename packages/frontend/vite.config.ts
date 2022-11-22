33323cf0308c88d05dad1b4ac7b3adde538085c9
Denis Avakov <denis.avakov@pm.me>
2022-11-22 16:53:41 +0200

    feat: scaffolding a frontend directory

---
+++ packages/frontend/vite.config.ts
@@ -0,0 +1,9 @@
+// Include your project-specific settings in this file
+// Read about how to configuring Vite works: https://vitejs.dev/config/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()]
+});
